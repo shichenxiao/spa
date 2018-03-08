4febaac4220451bbc97cb14a3bcd644bb8a6b41d
shichenxiao <1739118128@qq.com>
2018-03-08 19:30:27 +0800

    第三次提交

--- 01-first-app/calculate.js
+++ 01-first-app/calculate.js
@@ -1,3 +1,20 @@
-function(){
+$(function(){
+    $("#wide").blur(function(){
+       if($("#wide").val()==""){
 
-}
+         $("#wide").next("span").html("输入不能为空！");
+
+       } 
+    });
+
+  $("#add").attr("disabled","false").css("background","#E0E0E0");
+  $("#mutiply").attr("disabled","false").css("background","#E0E0E0")
+  $("#btn").click(function(){
+     var w = $("#wide").val();
+     var h = $("#high").val();
+     $("#add").val(w*2+h*2);
+     $("#mutiply").val(w*h);
+
+
+  });
+});
