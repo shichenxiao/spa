a78da42f2be76c8603ffa0c1ee3cd7c98f034b9b
shichenxiao <1739118128@qq.com>
2018-03-08 12:20:48 +0800

    修改js文件

--- 01-first-app/index.js
+++ 01-first-app/index.js
@@ -7,7 +7,11 @@ $(function(){
       $('.perimeter').val(perimeter(width,height));
       $('.area').val(area(width,height));
     });
-    
+    $width.focusout(function(){
+      validate('.width');
+    });
+$height.focusout(function(){
+  validate('.height');
 });
 
 function perimeter(x,y){
@@ -22,5 +26,30 @@ function area(x,y){
   var max = Math.max(lenX,lenY);
   return (x*(10**max)+y*(10**max))/(10**max);
 }
+function validate(field){
+  var $data = $(field),
+      $message = $(field+'-validate'),
+      label = $(field).attr('data-label');
 
+  if($data.val() === ''){
+    $message.html(label + '不能为空！');
+    $data.select();
+    return;
+  }
+
+  if(!/^-?(0|[1-9]\d*)(\.\d*)?([eE][+-]?\d+)?$/.test($data.val())){
+    $message.html(label + '必须是数值');
+    $data.select();
+    return;
+  }
+
+  if(window.Number($data.val())<0){
+    $message.html(label + '必须大于零');
+    $data.select();
+    return;
+  }
+
+  $message.html('');
+}
+});
 
