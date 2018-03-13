f4f576ad4d335e01c19c4f5ac9c9c77e6d388081
shichenxiao <1739118128@qq.com>
2018-03-13 11:04:14 +0800

    修改05.js

--- 05-progress/progress.js
+++ 05-progress/progress.js
@@ -12,7 +12,7 @@ function pause(){
 function reset(){
   var pg = document.getElementById('pg');
   pg.value=0;
-  setInterval(function(e){
+  bar = setInterval(function(e){
     if(pg.value!=100) pg.value++;
     else pg.value=100;
   },100);
